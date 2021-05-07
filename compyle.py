c2725b3e944c1013b0afb227b6973beaa1049f41
Thijs Boom <twboom@hotmail.nl>
2021-05-07 22:21:28 +0200

    Did some work on getting the files

--- compyle.py
+++ compyle.py
@@ -1,9 +1,31 @@
 import sys
 import os
 
-config = open('pages.config', 'r').readlines()
-config = [i.strip() for i in config]
+# Compile the files
+def compilePage(file):
+    pass
 
-print(config)
 
-print(sys.argv)
+# Get the files to compile
+def getFile(dir):
+    struct = dir.split('/')
+
+    file = struct[-1] # Filename and extension
+    del struct[-1] # Folders in a list
+
+    path = '/'
+    path = path.join(struct) # Folders in a strings, seperated with a '/'
+    print(path)
+
+
+# Export the files to the output folder
+def exportPage(content, location, name, extension):
+    pass
+
+
+
+# Importing the list of pages to generate
+pages = open('pages.config', 'r').readlines()
+pages = [i.strip() for i in pages]
+
+getFile(pages[0])
